d81a2bad41963564d31de261649c5083d02499db
Iman Amirul <iman@imanamirul.uitm.edu>
2021-01-09 00:23:42 +0800

    server is updated, comments added

--- s.py
+++ s.py
@@ -2,38 +2,42 @@ import socket
 import threading 
 from Crypto.Cipher import AES
 
+# FUNCTION for ENCRYPTION
 def encrypt(encrypt_data):
     obj = AES.new(b"1122334456789001", AES.MODE_CFB, b"2299225510784791")
     data = obj.encrypt(encrypt_data)
     return data
 
+# FUNCTION for DECRYPTION
 def decrypt(decrypt_data):
     obj = AES.new(b"1122334456789001", AES.MODE_CFB, b"2299225510784791")
     data = obj.decrypt(decrypt_data)
     return data
 
+# SERVER address and PORT number, fill in beforehand
 PORT = 5050
- 
-SERVER = "192.168.42.152" #socket.gethostbyname(socket.gethostname()) 
-  
+SERVER = socket.gethostbyname(socket.gethostname()) 
+
 addr = (SERVER, PORT) 
  
 sixt = "utf-8"
-  
+
+# LIST of CLIENTS and their NAMES for multithreading
 clients, names = [], [] 
 
+# BIND the SERVER and PORT to SOCKET
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    
-server.bind(addr) 
- 
-def startChat(): 
-    
-    print("server is working on " + SERVER) 
-        
+server.bind(addr)
+
+# FUNCTION for CHATROOM connection
+def startChat():
+
+    print("\n\n [+] CHATROOM SERVER is up and running : " + SERVER) 
+
     server.listen(5) 
-      
-    while True: 
-        
+
+    while True:
+
         conn, addr =  server.accept() 
         conn.send(encrypt("NAME")) 
 
@@ -44,34 +48,38 @@ def startChat():
         names.append(name) 
         clients.append(conn) 
           
-        print(f"Name is :{name}") 
+        print(f" [+] Name :{name}") 
           
 
-        broadcastMessage(f"{name} has joined the chat!") 
+        broadcastMessage(f" [+] {name} has joined the chat!") 
           
-        conn.send(encrypt('Connection successful!'))
+        conn.send(encrypt(' [+] Connection successful!'))
 
         thread = threading.Thread(target = handle, args = (conn, addr)) 
         thread.start() 
- 
-        print(f"active connections {threading.activeCount()-1}") 
-  
+
+        print(f" [+] Active connections : {threading.activeCount()-1}") 
+
+# FUNCTION to hande incoming connection and messages 
 def handle(conn, addr): 
-    
-    print(f"new connection {addr}") 
+
+    print(f" [+] New connection : {addr}") 
     connected = True
-      
+
     while connected: 
 
-        msg = conn.recv(1024) 
+        msg = conn.recv(1024)
         temp = decrypt(msg)
-        message = temp.decode()  
-        broadcastMessage(message) 
-       
+        message = temp.decode()
+        broadcastMessage(message)
+
     conn.close() 
- 
-def broadcastMessage(message): 
+
+# FUNCTION to send and display messages to other CLIENTS
+def broadcastMessage(message):
     for client in clients:
         msg = encrypt(message)
-        client.send(msg)  
+        client.send(msg)
+
+# FUNCTION call for  CHATROOM server
 startChat() 
