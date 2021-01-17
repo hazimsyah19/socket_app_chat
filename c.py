b6ed23205ced1a3ce113686a12eedab7e45ed12c
Iman Amirul <iahadzim@gmail.com>
2021-01-17 14:50:30 +0800

    updated client and server to current state

--- c.py
+++ c.py
@@ -1,5 +1,5 @@
 import socket
-import threading 
+import threading
 from tkinter import *
 from tkinter import font 
 from tkinter import ttk
@@ -8,7 +8,7 @@ from Crypto.Cipher import AES
 
 # SERVER address and PORT number, fill in beforehand
 PORT = 5050
-SERVER = "192.168.0.117"
+SERVER = "192.168.0.104"
 ADDRESS = (SERVER, PORT) 
 sixt = "utf-8"
   
@@ -41,19 +41,19 @@ class GUI:
         self.login = Toplevel()
 
         # TITLE for the LOG IN display
-        self.login.title("    LOG IN")
+        self.login.title("    CHAT HUB")
         self.login.resizable(width = 0, height = 0)
         self.login.configure(width = 300, height = 400, bg = "#1A1A1A")
 
-	# CHATROOM APP LOGO, set the logo's directory beforehand
-        image0 = Image.open("/home/iman//CHAT/socket_app_chat/161026056787944340.png")
+        # CHATROOM APP LOGO, set the logo's directory beforehand
+        image0 = Image.open("/root/Documents/python/socket_chat_app/161026056787944340.png")
         image1 = image0.resize((120,120), Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(image1)
         self.canvas = Label(self.login, width = 120, height = 120,image = self.img, bg = "#1A1A1A")
         self.canvas.image = self.img
         self.canvas.place(x = 80, y = 10)
 
-	# LABEL for entering CHATROOM by sending NAME to the server
+        # LABEL for entering CHATROOM by sending NAME to the server
         self.labelLogin = Label(self.login, text = "To join the chat room,\n enter your name", 
                        justify = CENTER, font = "Verdana 14 bold", fg = "#F8F8F8", bg = "#1A1A1A") 
         self.labelLogin.place(relheight = 0.3, relx = 0.1, rely = 0.28) 
@@ -71,7 +71,7 @@ class GUI:
                          command = lambda: self.enterCHAT(self.entryName.get()))
         self.sendName.place(relwidth = 0.5, relheight = 0.15, relx = 0.25, rely = 0.75)
 
-	# LOOP for ROOT
+        # LOOP for ROOT
         self.root.mainloop()
 
     # FUNCTION to enter CHATBOX and create thread to receive messages
@@ -90,7 +90,7 @@ class GUI:
 
         # Display CHATBOX 
         self.root.deiconify() 
-        self.root.title("CHATROOM") 
+        self.root.title("CHATHUB") 
         self.root.resizable(width = 0, height = 0) 
         self.root.configure(width = 470, height = 530, bg = "#1A1A1A")
 
@@ -108,7 +108,7 @@ class GUI:
         self.labelBottom = Label(self.root, bg = "#4E4E4E", height = 75) 
         self.labelBottom.place(relwidth = 1, rely = 0.825)
 
-	# ENTRY for message
+        # ENTRY for message
         self.entryMsg = Entry(self.labelBottom, bg = "#F8F8F8", fg = "#1A1A1A", font = "Verdana 13")
         self.entryMsg.place(relwidth = 0.74, relheight = 0.05, rely = 0.0075, relx = 0.011)
         self.entryMsg.focus()
@@ -133,7 +133,7 @@ class GUI:
         self.textCons.config(state = DISABLED) 
         self.msg=msg 
         self.entryMsg.delete(0, END) 
-        snd = threading.Thread(target = self.sendMessage) 
+        snd= threading.Thread(target = self.sendMessage) 
         snd.start() 
 
     # FUNCTION to receive messages from the server
@@ -143,15 +143,15 @@ class GUI:
                 msg = client.recv(2048)
                 temp = decrypt(msg)
                 message = temp.decode()
-                # If the messages from the server is NAME then send the client's name 
-                if message == 'NAME': 
+                # SEND CLIENT's NAME if the server ask for ot
+                if message == 'NAME':
                     client.send(encrypt(self.name))
+                # CLIENT QUIT
                 elif message == '!bye':
                     self.textCons.config(state = NORMAL) 
                     self.textCons.insert(END, message+"\n") 
                     self.textCons.config(state = DISABLED) 
-                    self.textCons.see(END)
-                    client.close()
+                    self.textCons.see(END,client.close())
                     self.root.destroy()
                 else: 
                     # INSERT messages into CHATBOX
@@ -169,7 +169,7 @@ class GUI:
     def sendMessage(self):
         self.textCons.config(state=DISABLED) 
         while True: 
-            msg = (f" {self.name} : {self.msg}") 
+            msg = (f"{self.name}: {self.msg}") 
             message = encrypt(msg)
             client.send(message)
             break
--- s.py
+++ s.py
@@ -1,8 +1,9 @@
-import socket 
-import threading 
+import socket
+import threading
 from Crypto.Cipher import AES
 from os import system, name
 
+# FUNCTION for clear screen on the terminal
 def clear():
     if name == 'nt':
         _ = system('cls')
@@ -23,17 +24,17 @@ def decrypt(decrypt_data):
 
 # SERVER address and PORT number, fill in beforehand
 PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname()) 
+SERVER = socket.gethostbyname(socket.gethostname())
 
-addr = (SERVER, PORT) 
- 
+addr = (SERVER, PORT)
+
 sixt = "utf-8"
 
 # LIST of CLIENTS and their NAMES for multithreading
 clients ,names = [] ,[]
 
 # BIND the SERVER and PORT to SOCKET
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(addr)
 
 # FUNCTION for CHATROOM connection
@@ -41,61 +42,63 @@ def startChat():
 
     clear()
 
-    print("\n [+] CHATROOM SERVER is up and running : " + SERVER) 
+    print("\n [+] CHATROOM SERVER is up and running : " + SERVER)
 
-    print("\n [+] CHATROOM SERVER is waiting for connection... \n")
-    server.listen(5) 
+    print("\n [+] CHATROOM SERVER is waiting for connection... ")
+    server.listen(5)
 
     while True:
 
-        conn, addr =  server.accept() 
-        conn.send(encrypt("NAME")) 
+        conn, addr =  server.accept()
+        conn.send(encrypt("NAME"))
 
         n = conn.recv(2048)
         temp = decrypt(n)
         name = temp.decode()
- 
-        names.append(name) 
-        clients.append(conn) 
-          
-        print(f"\n [+] Name : {name}") 
-          
 
-        broadcastMessage(f"\n [+] {name} has joined the chat!") 
-          
-        conn.send(encrypt(' [+] Connection successful! Enter !bye to quit. \n'))
+        names.append(name)
+        clients.append(conn)
+
+        print(f" [+] Name :{name}")
+
+        broadcastMessage(f"\n [+] {name} has joined the chat!")
+
+        conn.send(encrypt(' [+] Connection successful! \n'))
 
         thread = threading.Thread(target = handle, args = (conn, addr,name))
-        thread.start() 
+        thread.start()
 
-        print(f" [+] Active connections : {threading.activeCount()-1}") 
+        print(f" [+] Active connections : {threading.activeCount()-1}")
 
-# FUNCTION to hande incoming connection and messages 
+# FUNCTION to hande incoming connection and messages
+def handle(conn, addr , name):
 
-def handle(conn, addr , name): 
 
-
-    print(f" [+] New connection : {addr}") 
+    print(f" [+] New connection : {addr}")
     connected = True
 
     while connected:
 
         msg = conn.recv(1024)
+	print(msg)
         temp = decrypt(msg)
         message = temp.decode()
-        #print(len(message))
+        # PRINT(len(message))
         g = message[message.index(" ") + 1:]
         if g != '!bye':
             broadcastMessage(message)
         else:
-            broadcastMessage('\n [+] %s left the chat' % name)
+            broadcastMessage('%s left the chat' % name)
             conn.close()
+            server.close()
             break
 
 # FUNCTION to send and display messages to other CLIENTS
 def broadcastMessage(message):
     for client in clients:
         msg = encrypt(message)
+	print(msg)
         client.send(msg)
+
 # FUNCTION call for  CHATROOM server
-startChat() 
+startChat()
